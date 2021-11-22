15a0bec97e2ab06abf770eddd9789a4b1b890c9e
María Delfina Ramos Vidal <delfina.ramos@udc.es>
2021-11-22 09:45:43 +0100

    Update goDocker.sh

--- goDocker.sh
+++ goDocker.sh
@@ -2,10 +2,10 @@ docker stop tdbdocker
 
 docker rm tdbdocker
 
-docker build -t jenatdb --rm=true .
+docker build -t jenatdb --rm=true --no-cache=true .
 
-docker run --name tdbdocker -it jenatdb
+#docker run --name tdbdocker -it jenatdb
 
-# docker run --name tdbdocker --mount type=bind,source="$(pwd)/data", target=/data -it jenatdb
+docker run --name tdbdocker --mount type=bind,source="$(pwd)/../data",target=/data -it jenatdb
 
-#docker exec -it tdbdocker /bin/bash
+docker exec -it tdbdocker /bin/bash
